bca46c4266808fbb2ffe2e45e90d2b652b5daad7
sashwatjain <sashwatjain1@gmail.com>
2025-11-26 14:35:21 +0530

    Implement subarray sum and page allocation functions in solution.py

--- dsa/day_10/problem.md
+++ dsa/day_10/problem.md
@@ -1,3 +1,6 @@
 # Problem url sheet
 
 https://docs.google.com/spreadsheets/d/1V5dedwFYWzyuaZTgGfZbLmacq1ma0yjUIm-TvKEGSbc/edit?gid=0#gid=0
+
+	Print Subarrays with 0 Sum
+	Aggressive Cows
--- dsa/day_10/solution.py
+++ dsa/day_10/solution.py
@@ -5,9 +5,46 @@ Difficulty:
 Date:
 """
 
-class Solution:
-    def solve(self, *args, **kwargs):
-        pass
+def findSubArrays(arr,  n):
+
+    # Array to store all the start and end
+    # indices of subarrays with 0 sum
+    out = []
+    i = 0
+    while (i < n):
+        prefix = 0
+        j = i
+        while (j < n):
+            prefix += arr[j]
+            if (prefix == 0):
+                out.append(Pair(i, j))
+            j += 1
+        i += 1
+    return out
+
+
+def findPages(self, arr, k):
+    if len(arr)<k:
+        return -1
+    l,h,ans=0,sum(arr),-1
+    while l<=h:
+        mid=(l+h)//2
+        if self.isPossible(arr,mid,k):
+            ans=mid
+            h=mid-1
+        else:
+            l=mid+1
+    return ans
+def isPossible(self,arr,val,k):
+    count,curr=1,0
+    for pages in arr:
+        curr+=pages
+        if curr>val:
+            curr=pages
+            count+=1
+        if count>k or pages>val:
+            return False
+    return True
 
 # Approach:
 # Time Complexity:
