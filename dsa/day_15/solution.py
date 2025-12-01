db076e0a5bf2001e21d1e3bd46c658a2bfafef69
Sashwat Jain <sashwatjain1@gmail.com>
2025-12-01 22:38:18 +0530

    Update solution.py

--- dsa/day_15/solution.py
+++ dsa/day_15/solution.py
@@ -21,7 +21,21 @@ class Solution:
         
          return node
             
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while(fast and fast.next):
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast :
+                return True
+        return False        
 # Approach:
 # Time Complexity:
 # Space Complexity:
